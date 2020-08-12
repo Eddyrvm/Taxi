c243ebc2a865a3292a05e539f5934b245e14c12e
Eddy Vera Mejia <nayo_ec@outlook.com>
2020-08-11 23:05:10 -0500

    CREACION DEL USER HELPER

---
+++ Taxi.Web/Helpers/UserHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Helpers
+{
+    public class UserHelper : IUserHelper
+    {
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserHelper(UserManager<UserEntity> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<IdentityResult> AddUserAsync(UserEntity user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddUserToRoleAsync(UserEntity user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CheckRoleAsync(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(UserEntity user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
